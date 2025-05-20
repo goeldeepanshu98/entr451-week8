0600820d266c1b333d4ef7a0b8cf6f297f6b3295
Deepanshu Goel <deepanshu.goel@kellogg.northwestern.edu>
2025-05-20 16:29:35 +0000

    done

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,7 +11,7 @@ class SessionsController < ApplicationController
     if @user != nil
       # 3. if they know their password -> login is successful
       if BCrypt::Password.new(@user["password"]) == params["password"]
-        cookies["monster"] = "I love cookies"
+        session["user_id"] = @user["id"]
         flash["notice"] = "Welcome."
         redirect_to "/companies?logged_in_user_id=#{@user["id"]}"
       else
@@ -28,6 +28,7 @@ class SessionsController < ApplicationController
   def destroy
     # logout the user
     flash["notice"] = "Goodbye."
+    session["user_id"] = nil
     redirect_to "/login"
   end
 end
